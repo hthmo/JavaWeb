3733465edc85f13110100e80d849fad8f39c3321
hthmo <90694536+hthmo@users.noreply.github.com>
2022-04-08 13:37:25 +0700

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -1,5 +1,10 @@
 package com.example.reponsitory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class UserRepository {
+import net.javaguides.springboot.model.User;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+	User findByEmail(String email);
 }
