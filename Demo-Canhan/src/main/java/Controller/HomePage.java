c6bf574e533b07696b820791c9f707c698638281
hthmo <90694536+hthmo@users.noreply.github.com>
2022-04-22 14:36:59 +0700

    Update HomePage.java

--- Demo-Canhan/src/main/java/Controller/HomePage.java
+++ Demo-Canhan/src/main/java/Controller/HomePage.java
@@ -1,5 +1,42 @@
 package Controller;
 
-public class HomePage {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePage {
+	@GetMapping("/")
+	public String HomePage(Model model) 
+	{
+		return "TrangChu";
+	}
+	
+	@GetMapping("/admin") 
+    public String admin() {
+        return "admin";
+    }
+	@GetMapping("/403")
+    public String accessDenied() {
+        return "403";
+    }
+
+    @GetMapping("/login") 
+    public String getLogin() {
+        return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
+    }
 }
