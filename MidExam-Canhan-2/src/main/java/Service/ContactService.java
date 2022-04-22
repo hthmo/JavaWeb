cc856c7b6485dd2d8cdde9838a775b5b1486cc59
hthmo <90694536+hthmo@users.noreply.github.com>
2022-04-22 14:41:45 +0700

    Update ContactService.java

--- MidExam-Canhan-2/src/main/java/Service/ContactService.java
+++ MidExam-Canhan-2/src/main/java/Service/ContactService.java
@@ -1,5 +1,19 @@
 package Service;
 
-public class ContactService {
+import Entity.Contact;
+
+import java.util.List;
+
+public interface ContactService {
+
+    Iterable<Contact> findAll(); // Tim toan bo
+
+    List<Contact> search(String term);
+
+    Contact findOne(Integer id);// Tim 1
+
+    void save(edu.till.Homestay.Contact contact); // Them 1 lien lac
+
+    void delete(Integer id);	 // Xoa 1 lien lac
 
 }
