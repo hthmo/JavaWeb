9782399ef80e3f448a6dca02da1289910db149d2
hthmo <90694536+hthmo@users.noreply.github.com>
2022-04-22 11:38:09 +0700

    first commit

---
+++ Homestaycasabella/src/main/java/edu/till/Homestay/HomestaycasabellaApplication.java
@@ -0,0 +1,13 @@
+package edu.till.Homestay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomestaycasabellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomestaycasabellaApplication.class, args);
+	}
+
+}
---
+++ Homestaycasabella/src/test/java/edu/till/Homestay/HomestaycasabellaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.till.Homestay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomestaycasabellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
