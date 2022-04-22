e141cb16a967355ad050d87ce34f10a11b90d3e2
hthmo <90694536+hthmo@users.noreply.github.com>
2022-04-22 11:49:28 +0700

    first commit

---
+++ QLDangnNhap_PhanQuyen/src/main/java/edu/till/QLBanHang/QlDangnNhapPhanQuyenApplication.java
@@ -0,0 +1,13 @@
+package edu.till.QLBanHang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlDangnNhapPhanQuyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlDangnNhapPhanQuyenApplication.class, args);
+	}
+
+}
---
+++ QLDangnNhap_PhanQuyen/src/test/java/edu/till/QLBanHang/QlDangnNhapPhanQuyenApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.till.QLBanHang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlDangnNhapPhanQuyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
